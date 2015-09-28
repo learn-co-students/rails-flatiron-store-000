ea53ffff2f73213129130505a77d7d7477b8a5ea
garettarrowood <garettarrowood@gmail.com>
2015-09-28 12:40:01 -0400

    need help

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,26 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
 
+  def total
+    price = 0
+    items.each do |item|
+      price = price + item.price
+    end
+    price
+  end
+
+  def add_item(item_id)
+    # binding.pry
+    line_items.each do |li|
+      # binding.pry
+      if item_id == li.item_id
+        new_quantity = li.quantity + 1
+        li.destroy
+        # binding.pry
+        return LineItem.create(cart_id: id, item_id: item_id, quantity: new_quantity)
+      end
+    end
+    LineItem.create(cart_id: id, item_id: item_id)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,16 @@
 class Order < ActiveRecord::Base
+  belongs_to :cart
+  has_many :items, through: :cart
+
+  def create_from_cart
+  end
+
+  def change_order_status
+    status = "submitted"
+  end
+
+  def change_inventory
+    cart.itmes.inventory = cart.items.inventory - 1
+  end
 
 end
